0f759d24eeed9937f7576b2ae620eec8a4607778
shotmeow <shotmeow@gmail.com>
2023-02-23 00:15:20 +0300

    Fix Overflow X Scrolling

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/modules/Header";
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="h-full bg-white dark:bg-gradient-to-t from-bg-dark to-bg-light text-black dark:text-white">
+    <div className="h-full bg-white dark:bg-gradient-to-t from-bg-dark to-bg-light text-black dark:text-white overflow-x-hidden">
       <div className="container mx-auto">
         <Header />
         {children}
