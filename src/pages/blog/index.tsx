5f5f650c09c2d45c9d7a57a9870bf5a8ed2440df
ShotMeow <shotmeow@gmail.com>
2023-02-22 10:53:52 +0300

    Add Title for Pages

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { NextPage } from "next";
 import Layout from "@/components/Layout/Layout";
+import Head from "next/head";
 
 const BlogPage: NextPage = () => {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <Head>
+        <title>Данила Маврин - Блог</title>
+      </Head>
+    </Layout>
+  );
 };
 
 export default BlogPage;
--- src/pages/resources/index.tsx
+++ src/pages/resources/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { NextPage } from "next";
 import Layout from "@/components/Layout/Layout";
+import Head from "next/head";
 
 const ResourcesPage: NextPage = () => {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <Head>
+        <title>Данила Маврин - Ресурсы</title>
+      </Head>
+    </Layout>
+  );
 };
 
 export default ResourcesPage;
