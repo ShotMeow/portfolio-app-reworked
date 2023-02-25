900d4833521fcebe830be0866b4180e6ee84f44b
shotmeow <shotmeow@gmail.com>
2023-02-25 11:32:01 +0300

    Refactor: Change Favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,6 +18,23 @@ const Document: NextPage<DocumentType> = () => {
           href="https://fonts.gstatic.com"
           crossOrigin={"anonymous"}
         />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/favicon-180x180.png"
+        />
       </Head>
       <body>
         <Main />
