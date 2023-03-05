cf7828f37c60fb9c6e5e332b94cc29ddcd60a281
shotmeow <shotmeow@gmail.com>
2023-03-05 18:48:20 +0300

    Added: Padding for List

--- src/components/templates/Training/Sections/WhatYouGet/WhatYouGet.tsx
+++ src/components/templates/Training/Sections/WhatYouGet/WhatYouGet.tsx
@@ -6,7 +6,7 @@ const WhatYouGet: FC = () => {
   return (
     <motion.section className="space-y-6" {...inViewFromBottomAnimation}>
       <h3 className="text-4xl text-center">Что вы получите</h3>
-      <ul className="text-xl font-thin list-disc space-y-4">
+      <ul className="text-xl font-thin list-disc space-y-4 pl-4">
         <li>
           Индивидуального преподавателя, сопровождающего вас на пути к
           трудоустройству.
