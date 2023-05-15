c713ce466652dda8000af31a1b092f331c151856
ShotMeow <shotmeow@gmail.com>
2023-05-15 15:49:27 +0300

    Refactor: Delete useless resources Added: Doka Guide resource

--- src/templates/Resources/data/index.ts
+++ src/templates/Resources/data/index.ts
@@ -2,6 +2,12 @@ import { ResourceType } from "../types";
 
 export const resourcesData: ResourceType[] = [
 	{
+		label: "Doka Guide",
+		description:
+			"Лучшая документация про веб на русском языке",
+		href: "https://doka.guide/",
+	},
+	{
 		label: "Frontend Mentor",
 		description:
 			"Сервис, предоставляющий практические задания по разработке интерфейсов.",
@@ -23,11 +29,6 @@ export const resourcesData: ResourceType[] = [
 		href: "https://convertio.co/ru/",
 	},
 	{
-		label: "Carbon",
-		description: "Сервис, превращающий код в красивую презентацию.",
-		href: "https://carbon.now.sh/",
-	},
-	{
 		label: "Metatags.io",
 		description: "Набор инструментов, позволяющих проводить различные эксперименты с контентом, перед заливкой его в Twitter или LinkedIn, с функцией предварительного просмотра.",
 		href: "https://metatags.io/"
@@ -36,26 +37,6 @@ export const resourcesData: ResourceType[] = [
 		label: "Readme.so",
 		description: "Удобный редактор README.md файла",
 		href: "https://readme.so/ru"
-	},
-	{
-		label: "Logoipsum",
-		description: "Сервис, предоставляющий огромное количество бесплатных логотипов.",
-		href: "https://logoipsum.com/"
-	},
-	{
-		label: "synthesia",
-		description: "Площадка по созданию видеороликов при помощи ИИ, поддерживающая почти 50 языков мира.",
-		href: "https://www.synthesia.io/"
-	},
-	{
-		label: "Peppertype",
-		description: "Цифровой помощник по производству контента, инструменты которого разработаны на основе машинного обучения, с применением искусственного интеллекта, автоматизирующего все процессы создания задуманного.",
-		href: "https://www.peppercontent.io/peppertype-ai/"
-	},
-	{
-		label: "Storytale",
-		description: "Удобный ресурс, хранящий в себе множество бесплатных иллюстраций на любой вкус.",
-		href: "https://storytale.io/"
 	},
 	{
 		label: "resume.io",
